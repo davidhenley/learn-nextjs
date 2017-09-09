bed99300aed2e1293ebd41bef7ed47218133df7e
David Henley <henleydr@gmail.com>
2017-09-09 18:19:14 -0500

    Added button to Link

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Index = () => (
   <div>
     <Link href="/about">
-      <a>About Page</a>
+      <button>Go to our about page</button>
     </Link>
     <p>Hello Next.js</p>
   </div>
